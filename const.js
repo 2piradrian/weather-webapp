c1264c68186fe4cf5b5db7f4b758bb9f4a56b55a
Adrian <rodriguezcadr@gmail.com>
2022-09-24 16:01:51 -0300

    local storage & url

--- const.js
+++ const.js
@@ -1,5 +1,26 @@
 const $cardContainer = document.querySelector(".cardContainer");
 const $submitCity = document.getElementById("submitCity");
 const $addCity = document.getElementById("addCity");
+const $addCityForm = document.getElementById("addCityForm");
 
 const API_KEY = "fe01eb2655029ceeaf5afb0a96514729";
+
+/* Requests */
+const requestCity = async (city) => {
+	// pre request ->
+	const url = `https://api.openweathermap.org/data/2.5/weather?lang=es&units=metric&q=${city}&appid=${API_KEY}`;
+	// request ->
+	const response = await fetch(url);
+	const data = await response.json();
+	return data;
+};
+const requestLocation = async () => {
+	// pre request ->
+	const position = navigator.geolocation.getCurrentPosition;
+	const { latitude, longitude } = position.coords;
+	const url = `https://api.openweathermap.org/data/2.5/weather?lang=es&units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+	// request ->
+	const response = await fetch(url);
+	const data = await response.json();
+	return data;
+};
--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 // traer elementos del local storage
 let cities = JSON.parse(localStorage.getItem("cities")) || [];
 
-// funcion para guargar elementos en local storage
+// funcion para guardar elementos en local storage
 const saveLocalStorage = (citiesList) => {
 	localStorage.setItem("cities", JSON.stringify(citiesList));
 };
+
+// funcion para redondear numeros
+const roundNumbers = (number) => {
+	return Math.round(number);
+};
+
+const renderCard = () => {};
