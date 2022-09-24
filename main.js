9f4e8444253c498cfa0d09e2feed34ce4831d463
Adrian <rodriguezcadr@gmail.com>
2022-09-24 16:26:01 -0300

    renderCards

--- main.js
+++ main.js
@@ -11,4 +11,43 @@ const roundNumbers = (number) => {
 	return Math.round(number);
 };
 
-const renderCard = () => {};
+const renderCard = (dataObj) => {
+	return `
+    <div class="container" id="yourCity">
+                    <i class="fa-solid fa-x" data-id="${dataObj.id}"></i>
+					<h5 class="city">${dataObj.name}, ${dataObj.sys.country}</h5>
+					<div class="mainInfo">
+						<div class="description">
+							<p class="timeDescription">${dataObj.weather.description}</p>
+							<div class="temps">
+								<p class="feelsLike">S.T. ${roundNumbers(dataObj.main.feels_Like)}º</p>
+								<p class="temperature">${roundNumbers(dataObj.main.temp)}º</p>
+							</div>
+							<div class="minmax">
+								<i class="fa-solid fa-arrow-down"></i>
+								<p class="min">${roundNumbers(dataObj.main.temp_min)}º</p>
+								<i class="fa-solid fa-arrow-up"></i>
+								<p class="max">${roundNumbers(dataObj.main.temp_max)}º</p>
+							</div>
+						</div>
+						<div class="imgContainer">
+							<img src="/assets/${dataObj.weather[0].icon}.png" alt="Weather image" srcset="" />
+						</div>
+					</div>
+					<div class="secondaryInfo">
+						<div class="humidityInfo">
+							<i class="fa-solid fa-droplet"></i>
+							<p class="humidity">${dataObj.main.humidity}%</p>
+						</div>
+						<div class="pressureInfo">
+							<i class="fa-solid fa-hurricane"></i>
+							<p class="pressure">${dataObj.main.pressure}hP</p>
+						</div>
+						<div class="windInfo">
+							<i class="fa-solid fa-wind"></i>
+							<p id="wind">${dataObj.wind.speed}Km/h</p>
+						</div>
+					</div>
+				</div>
+    `;
+};
